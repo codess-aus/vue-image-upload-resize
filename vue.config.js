52799eceecc92f6065c07e0ab959eaf6f14bec05
Michelle Mei-Ling Sandford <msandford@outlook.com>
2021-11-07 15:21:14 +0800

    initial push
    Some PWA edits, but original site intact

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 // vue.config.js
+const manifestJSON = require('./public/manifest.json');
+module.exports = {
+	pwa: {
+		name: manifestJSON.short_name,
+		msTileColor: manifestJSON.background_color,
+		appleMobileWebAppCapable: 'yes',
+		appleMobileWebAppStatusBarStyle: 'black',
+		workboxPluginMode: 'InjectManifest',
+		workboxOptions: {
+			exclude: [/\.map$/, /web\.config$/],
+			swSrc: 'src/service-worker.js',
+		},
+	},
+};
+
+
+/*
 module.exports = {
   productionSourceMap: false,
   configureWebpack: {
@@ -7,3 +24,5 @@ module.exports = {
     },
   },
 }
+
+*/
